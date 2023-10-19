0f0d02e63281612706be0e3b19b747f776a3677b
BellaBradbury <bellabradbury@yahoo.com>
2023-10-19 05:18:57 -0400

    game loops but variables not retained

--- guessing_game.py
+++ guessing_game.py
@@ -1,7 +1,7 @@
 # IMPORTED MODULES
 import random
 
-    # random number chosen
+# SELECTS AND RETURNS A FRESH RANDOM NUMBER
 def get_rand_num(past_nums) :
     rand_num = random.randint(1, 10)
     past_nums.append(rand_num)
@@ -11,33 +11,39 @@ def get_rand_num(past_nums) :
     print('NUMBER: {}'.format(rand_num))
     return rand_num
 
+# CONGRATULATES THE USER ON A WIN, PROVIDES STATS, & PROMPTS TO CONTINUE
 def handle_win(loop_count, high_score, game_count) :
     print("\n*** --- !CONGRATULATIONS - YOU'VE GUESSED CORRECTLY! --- ***\n")
     final_score = loop_count + 1
     if game_count == 1 :
         high_score = final_score
-        print("You've set a new high score!")
     else :
         if final_score < high_score :
             high_score = final_score
-        print("You've set a new high score!")
+            print("You've set a NEW HIGH SCORE!")
     print( 'STATS:\n--High Score: {}\n--Final Score: {}\n--Total Game Rounds: {}'.format(high_score, final_score, game_count) )
 
     win_prompt = raw_input('\nPlay again?(yes/no)  ')
-    print(win_prompt)
-
+    game_count += 1
+    stats_arr = [3, high_score, final_score, game_count]
     if win_prompt.upper() == 'YES' :
-        game_count += 1
-        stats_arr = [1, high_score, final_score, game_count]
+        stats_arr[0] = 1
+        print('GOING AGAIN', stats_arr)
         return stats_arr
     elif win_prompt.upper() == 'NO' :
-        stats_arr = [0, high_score, final_score, game_count]
+        stats_arr[0] = 0
+        print('END', stats_arr)
         return stats_arr
 
-# continuously prompts user for a guess until win
+# PROMPTS USER FOR GUESS & HANDLES RESPONSE
 def game_loop(rand_num, game_count, high_score) :
-    print(rand_num)
+    print("\n*** --- NEW GAME --- ***")
     loop_count = 0
+
+    if high_score == 0 :
+        print('HIGH SCORE will be unlocked after first play through!\n')
+    else :
+        print( '\nHIGH SCORE: {}\n'.format(high_score) )
 
     while True :
         # provide feedback based on user input
@@ -64,6 +70,7 @@ def game_loop(rand_num, game_count, high_score) :
         #     print(err)
         #     continue
 
+# MANAGES VARIABLES & RUNS GAME
 def start_game() :
     # function variables
     past_nums = []
@@ -76,27 +83,22 @@ def start_game() :
         !!!  WELCOME & HAVE FUN  !!!
         *** -------------------- ***
     """)
-    if high_score == 0 :
-        print('HIGH SCORE will be unlocked after first play through!')
-    else :
-        print( 'HIGH SCORE: {}'.format(high_score) )
-        print( 'GAME COUNT: {}'.format(game_count) )
     print('\nTO PLAY:\n--Enter you guess of a number from 1-10 into the command line.\n--If you guess incorrectly you will be given a hint.\n--If you guess correctly the game will end and you can play again!')
 
+    # runs outside game functions
     rand_num = get_rand_num(past_nums)
-    print(rand_num)
-
     stats_arr = game_loop(rand_num, game_count, high_score)
-    print(stats_arr)
+    print('~~~LINE 1:', stats_arr)
 
-    if stats_arr[0] == 0 :
-        return stats_arr
-    else :
+    # if stats_arr[0] == 0 :
+    #     return stats_arr
+    while stats_arr[0] == 1 :
         high_score = stats_arr[1]
         game_count = stats_arr[3]
         rand_num = get_rand_num(past_nums)
-        stats_arr = game_loop(rand_num, game_count, high_score)
-        print(stats_arr)
+        print('ARR:', high_score, game_count, rand_num)
+        game_loop(rand_num, game_count, high_score)
+        print('ARR:', high_score, game_count, rand_num)    
     
 # calls function to start game
 stats_arr = start_game()
@@ -105,4 +107,3 @@ print("""
         !!!  THANKS FOR PLAYING  !!!
         *** -------------------- ***
     """)
-print(stats_arr)
