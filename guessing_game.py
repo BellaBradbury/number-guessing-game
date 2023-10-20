d2222291dcb7ad0bba8723fcbf1b17869a6b4144
BellaBradbury <bellabradbury@yahoo.com>
2023-10-20 00:16:40 -0400

    game loops & ends | HS & GC ?

--- guessing_game.py
+++ guessing_game.py
@@ -34,20 +34,12 @@ def handle_win(loop_count, high_score, game_count) :
         stats_arr[0] = 0
         print('END', stats_arr)
         return stats_arr
-
-# PROMPTS USER FOR GUESS & HANDLES RESPONSE
-def game_loop(rand_num, game_count, high_score) :
-    print("\n*** --- NEW GAME --- ***")
-    loop_count = 0
-
-    if high_score == 0 :
-        print('HIGH SCORE will be unlocked after first play through!\n')
-    else :
-        print( '\nHIGH SCORE: {}\n'.format(high_score) )
-
+    
+# GUESS LOOPS
+def guess_loop(loop_count, rand_num, game_count, high_score) :
     while True :
-        # provide feedback based on user input
-        # try:
+    # provide feedback based on user input
+        try:
             user_num = int( input('Pick a number from 1 to 10:  ') )
 
             if user_num < 1 or user_num > 10 :
@@ -61,16 +53,35 @@ def game_loop(rand_num, game_count, high_score) :
             elif user_num == rand_num :
                 stats_arr = handle_win(loop_count, high_score, game_count)
                 return stats_arr
-        # catches errors that arise from poor user input
-        # except NameError:
-        #     print('Input could not be read. Please enter only a whole number from 1 to 10.')
-        #     continue
-        # except SyntaxError as err:
-        #     print('Input could not be read. Please enter only a whole number from 1 to 10.')
-        #     print(err)
-        #     continue
+            # catches errors that arise from poor user input
+        except NameError:
+            print('Input could not be read. Please enter only a whole number from 1 to 10.')
+            continue
+        except SyntaxError as err:
+            print('Input could not be read. Please enter only a whole number from 1 to 10.')
+            print(err)
+            continue
 
-# MANAGES VARIABLES & RUNS GAME
+# PROMPTS USER FOR GUESS & HANDLES RESPONSE
+def game_loop(past_nums, game_count, high_score) :
+    while True : 
+        print("\n*** --- NEW GAME --- ***")
+        rand_num = get_rand_num(past_nums)
+        loop_count = 0
+
+        if high_score == 0 :
+            print('HIGH SCORE will be unlocked after first play through!\n')
+        else :
+            print( '\nHIGH SCORE: {}\n'.format(high_score) )
+
+        stats_arr = guess_loop(loop_count, rand_num, game_count, high_score)
+
+        if stats_arr[0] == 1 :
+            continue
+        else :
+            break
+
+# MANAGES VARIABLES & RUNS GAME FUNCTIONS
 def start_game() :
     # function variables
     past_nums = []
@@ -83,25 +94,28 @@ def start_game() :
         !!!  WELCOME & HAVE FUN  !!!
         *** -------------------- ***
     """)
-    print('\nTO PLAY:\n--Enter you guess of a number from 1-10 into the command line.\n--If you guess incorrectly you will be given a hint.\n--If you guess correctly the game will end and you can play again!')
+    print('\nTO PLAY:\n--Enter a number from 1-10 into the command line to make your guess.\n--If you guess incorrectly you will be given a hint.\n--If you guess correctly the game will end and you can play again!')
 
     # runs outside game functions
-    rand_num = get_rand_num(past_nums)
-    stats_arr = game_loop(rand_num, game_count, high_score)
-    print('~~~LINE 1:', stats_arr)
+    # rand_num = get_rand_num(past_nums)
+    # stats_arr = game_loop(rand_num, game_count, high_score)
+    # print('~~~LINE 1:', stats_arr)
 
     # if stats_arr[0] == 0 :
     #     return stats_arr
-    while stats_arr[0] == 1 :
-        high_score = stats_arr[1]
-        game_count = stats_arr[3]
-        rand_num = get_rand_num(past_nums)
-        print('ARR:', high_score, game_count, rand_num)
-        game_loop(rand_num, game_count, high_score)
-        print('ARR:', high_score, game_count, rand_num)    
+    # while stats_arr[0] == 1 :
+        # rand_num = get_rand_num(past_nums)
+    stats_arr = game_loop(past_nums, game_count, high_score)
+    print('~~~LINE 1:', stats_arr)
+        # high_score = stats_arr[1]
+        # game_count = stats_arr[3]
+        # rand_num = get_rand_num(past_nums)
+        # print('ARR:', high_score, game_count, rand_num)
+        # game_loop(rand_num, game_count, high_score)
+        # print('ARR:', high_score, game_count, rand_num)    
     
 # calls function to start game
-stats_arr = start_game()
+start_game()
 print("""
         *** -------------------- ***
         !!!  THANKS FOR PLAYING  !!!
